8b0b5d2f156d8a0d3456d01a033e933e5ff1d206
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2022-08-27 19:08:37 +0200

    fixup! feat(#2): :lipstick: styles

--- components/NestedDraggable.vue
+++ components/NestedDraggable.vue
@@ -37,25 +37,24 @@ const draggableComponent = computed(() => {
 </script>
 
 <template>
-    <Draggable
-      class="flex bg-gray-light dark:bg-black-dark"
-      :class="{
-        'flex-row h-full': type === 'column',
-        'flex-col gap-y-2 p-4  cursor-grab': type === 'task',
-      }"
-      tag="ul"
-      :list="items"
-      :group="{ name: type }"
-      item-key="name">
-      <template #item="{ element }">
-        <component :is="draggableComponent" :element="element">
-          <NestedDraggable
-            v-if="type === 'column'"
-            :items="element.tasks"
-            type="task"
-            @on-items-updated="(t) => $emit('onItemsUpdated', t)" />
-        </component>
-      </template>
-    </Draggable>
-
+  <Draggable
+    class="flex bg-gray-light dark:bg-black-dark"
+    :class="{
+      'flex-row h-full': type === 'column',
+      'flex-col gap-y-2 p-4 min-h-95% cursor-grab': type === 'task',
+    }"
+    tag="ul"
+    :list="items"
+    :group="{ name: type }"
+    item-key="name">
+    <template #item="{ element }">
+      <component :is="draggableComponent" :element="element">
+        <NestedDraggable
+          v-if="type === 'column'"
+          :items="element.tasks"
+          type="task"
+          @on-items-updated="(t) => $emit('onItemsUpdated', t)" />
+      </component>
+    </template>
+  </Draggable>
 </template>
